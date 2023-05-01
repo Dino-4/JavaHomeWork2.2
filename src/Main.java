3b7430ba76f1759630c3f682014db8e9e3405fa2
DiNo-4 <120940819+Dino-4@users.noreply.github.com>
2023-05-01 13:41:33 +0400

    origin

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int balanc_up_to = 352;
+        int balanceUpTo = 352;
         int accrued = 2200;
         int bonus = accrued / 100;
-        int balance = balanc_up_to + accrued + bonus;
+        int balance = balanceUpTo + accrued + bonus;
 
         System.out.println("Средств на счете: " + balance + " рубля(ей).");
     }
